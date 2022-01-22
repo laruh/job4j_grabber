6a6ea14a9fd2c37e05422a59f04752ce4bed1c90
laruh <oblomoff616@gmail.com>
2022-01-23 00:28:37 +0700

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,20 +7,16 @@ import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class SqlRuParse {
-    private final static String PATTERN = "(.*\\d{2}:\\d{2})";
-
     public static Post parsePost(Element href) throws IOException {
         String url = href.attr("href");
         Document doc = Jsoup.connect(url).get();
         String description = doc.select(".msgBody").get(1).text();
         SqlRuDateTimeParser parserTime = new SqlRuDateTimeParser();
         String time = doc.selectFirst(".msgFooter").text();
-        Matcher matcher = Pattern.compile(PATTERN).matcher(time);
-        LocalDateTime created = parserTime.parse(matcher.group());
+        String timeClean = time.substring(0, time.indexOf("[")).trim();
+        LocalDateTime created = parserTime.parse(timeClean);
         return new Post(href.text(), url, description, created);
     }
 
