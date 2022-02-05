0297c7d967dad41ae2694a38ef0d32f3137f0986
laruh <oblomoff616@gmail.com>
2022-02-06 05:20:34 +0700

    1. Хранилище продуктов [#852]

--- src/test/java/ru/job4j/ood/lsp/storage/ControllQualityTest.java
+++ src/test/java/ru/job4j/ood/lsp/storage/ControllQualityTest.java
@@ -10,24 +10,52 @@ import static org.junit.Assert.*;
 public class ControllQualityTest {
 
     @Test
-    public void foodSorted() {
+    public void foodShop() {
         Food milk = new Food("Milk new",
                 LocalDate.of(2022, 2, 10),
                 LocalDate.of(2022, 2, 4),
                 56.5);
-        Food soda = new Food("Soda new",
-                LocalDate.of(2022, 12, 2),
-                LocalDate.of(2021, 11, 2),
-                10);
-        Food badCheese = new Food("Bad Cheese",
-                LocalDate.of(2022, 2, 2),
-                LocalDate.of(2021, 1, 16),
-                10);
         Food chocolate = new Food("Chocolate",
                 LocalDate.of(2022, 2, 8),
                 LocalDate.of(2021, 1, 5),
                 10);
-        List<Food> foodList = List.of(milk, soda, badCheese, chocolate);
+        List<Food> foodList = List.of(milk, chocolate);
+
+        Warehouse warehouse = new Warehouse();
+        Shop shop = new Shop();
+        Trash trash = new Trash();
+
+        Predicate<Food> warehousePred = food -> warehouse.staleness(food) < 25;
+        Predicate<Food> trashPred = food -> trash.staleness(food) >= 100;
+        Predicate<Food> shopPred1 = food -> shop.staleness(food) >= 25
+                && shop.staleness(food) < 75;
+        Predicate<Food> shopPred2 = food -> shop.staleness(food) >= 75
+                && shop.staleness(food) < 100;
+
+        ControllQuality cQuality = new ControllQuality(foodList, warehouse, shop, trash);
+        cQuality.foodStorage(warehousePred, trashPred, shopPred1, shopPred2);
+
+        Food milkShop = new Food("Milk new",
+                LocalDate.of(2022, 2, 10),
+                LocalDate.of(2022, 2, 4),
+                56.5);
+        milkShop.setDiscount(0);
+        Food chocoShop = new Food("Chocolate",
+                LocalDate.of(2022, 2, 8),
+                LocalDate.of(2021, 1, 5),
+                10);
+        chocoShop.setDiscount(15);
+        List<Food> shopExp = List.of(milkShop, chocoShop);
+        assertEquals(shopExp, cQuality.getShop().findAll());
+    }
+
+    @Test
+    public void foodWarehouse() {
+        Food soda = new Food("Soda new",
+                LocalDate.of(2022, 12, 2),
+                LocalDate.of(2021, 11, 2),
+                10);
+        List<Food> foodList = List.of(soda);
 
         Warehouse warehouse = new Warehouse();
         Shop shop = new Shop();
@@ -48,26 +76,39 @@ public class ControllQualityTest {
                 LocalDate.of(2021, 11, 2),
                 10);
         sodaWare.setDiscount(0);
-        Food milkShop = new Food("Milk new",
-                LocalDate.of(2022, 2, 10),
-                LocalDate.of(2022, 2, 4),
-                56.5);
-        milkShop.setDiscount(0);
-        Food chocoShop = new Food("Chocolate",
-                LocalDate.of(2022, 2, 8),
-                LocalDate.of(2021, 1, 5),
+
+        List<Food> warehouseExp = List.of(sodaWare);
+        assertEquals(warehouseExp, cQuality.getWarehouse().findAll());
+    }
+
+    @Test
+    public void foodSorted() {
+        Food badCheese = new Food("Bad Cheese",
+                LocalDate.of(2022, 2, 2),
+                LocalDate.of(2021, 1, 16),
                 10);
-        chocoShop.setDiscount(15);
+        List<Food> foodList = List.of(badCheese);
+
+        Warehouse warehouse = new Warehouse();
+        Shop shop = new Shop();
+        Trash trash = new Trash();
+
+        Predicate<Food> warehousePred = food -> warehouse.staleness(food) < 25;
+        Predicate<Food> trashPred = food -> trash.staleness(food) >= 100;
+        Predicate<Food> shopPred1 = food -> shop.staleness(food) >= 25
+                && shop.staleness(food) < 75;
+        Predicate<Food> shopPred2 = food -> shop.staleness(food) >= 75
+                && shop.staleness(food) < 100;
+
+        ControllQuality cQuality = new ControllQuality(foodList, warehouse, shop, trash);
+        cQuality.foodStorage(warehousePred, trashPred, shopPred1, shopPred2);
+
         Food cheeseTrash = new Food("Bad Cheese",
                 LocalDate.of(2022, 2, 2),
                 LocalDate.of(2021, 1, 16),
                 10);
         cheeseTrash.setDiscount(0);
-        List<Food> warehouseExp = List.of(sodaWare);
-        List<Food> shopExp = List.of(milkShop, chocoShop);
         List<Food> trashExp = List.of(cheeseTrash);
-        assertEquals(warehouseExp, cQuality.getWarehouse().findAll());
-        assertEquals(shopExp, cQuality.getShop().findAll());
         assertEquals(trashExp, cQuality.getTrash().findAll());
     }
 }
